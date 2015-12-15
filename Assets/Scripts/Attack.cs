8a1440783bb02ad6f2cdc333463ad6512d6ac683
DasArmin <armin.hunter@hotmail.com>
2015-12-15 14:40:07 +0100

    Armin - Enemy Attack

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour
+{
+    private GameObject _foundPlayer;
+    [SerializeField] private LayerMask _playerMask;
+
+	void Update ()
+    {
+        FindPlayer();
+	}
+
+    void FindPlayer()
+    {
+        Collider[] _checkForPlayer = Physics.OverlapSphere(transform.position, 3f, _playerMask);
+
+        if (_checkForPlayer.Length == 0)
+            return;
+
+        _foundPlayer = _checkForPlayer[0].gameObject;
+
+        if (_foundPlayer != null)
+        {
+            print("Attack!");
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 3f);
+    }
+}
